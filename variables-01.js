46f718461ba2553aa4417de0ca666a57cc6977c8
lucis96 <112488759+Luisgrullon@users.noreply.github.com>
2022-09-18 14:41:36 -0400

    Add files via upload
    returning a string value for a welcome message

---
+++ variables-01.js
@@ -0,0 +1,18 @@
+/*Instructions
+ - Greet your friend by printing a message to the console.
+*/
+//your code here
+var myFriend = 'joel'
+///////////////
+function greetings() {
+  return'hello' // i only change this line of code to prove to employers
+  //things and get the hello greating
+}
+
+//leave this line unchanged to console log the results
+console.log('results: ', greetings());
+
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = greetings;
+}
